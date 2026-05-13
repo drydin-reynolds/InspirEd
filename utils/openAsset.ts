02fe796d6d43ee779a5b4e39437b4bb9d912a0c7
henry ison <thunderminer79@outlook.com>
2026-05-13 15:19:58 -0500

    fixed web side of modules

--- utils/openAsset.ts
+++ utils/openAsset.ts
@@ -1,8 +1,14 @@
+import { Platform } from "react-native";
 import * as FileSystem from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 
 export const openAssetPDF = async (name: string, filePath: string): Promise<void> => {
     try {
+        if (Platform.OS === "web") {
+            window.open(filePath, "_blank");
+            return;
+        }
+
         const localUri = FileSystem.documentDirectory + name + ".pdf";
 
         const { uri } = await FileSystem.downloadAsync(filePath, localUri);
